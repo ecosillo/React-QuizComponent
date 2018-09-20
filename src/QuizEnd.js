9e3ab7db03a98d6fe65587042dc0196dc01e9258
ecosillo <ecosillo@cosillo.com>
2018-09-20 00:30:55 -0500

    Completed the module

---
+++ src/QuizEnd.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class QuizEnd extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>Thanks for playing!</p>
+        <a onClick={this.handleResetClick.bind(this)} href=''>Reset Quiz</a>
+      </div>
+    )
+  }
+
+    handleResetClick(){
+      this.props.resetClickHandler()
+    }
+}
+
+export default QuizEnd
---
+++ src/QuizQuestion.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import QuizQuestionButton from './QuizQuestionButton.js'
+
+class QuizQuestion extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {incorrectAnswer : false}
+  }
+
+  render() {
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+        <ul>
+          {this.props.quiz_question.answer_options.map((answer_option, index) => {
+            return <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)}/>
+          })}
+        </ul>
+        </section>
+        {this.state.incorrectAnswer ? <p className='error'> Sorry, that''s not right.</p> : null }
+      </main>
+    )
+  }
+
+  handleClick(button_text){
+    if (button_text === this.props.quiz_question.answer) {
+      this.setState ((state) => {
+        return {incorrectAnswer : false}
+      })
+
+      this.props.showNextQuestionHandler();
+    }else {
+      this.setState ((state) => {
+        return { incorrectAnswer : true}
+      })
+    }
+  }
+}
+
+export default QuizQuestion
